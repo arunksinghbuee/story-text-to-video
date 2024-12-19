946585c732d71131c3c4e31c824e163cd48ece77
Arun Kumar Singh <arunkumar.singh@bt.com>
2024-12-19 23:15:58 +0530

    Search video

--- app.py
+++ app.py
@@ -24,18 +24,6 @@ if __name__ == "__main__":
     script = file.read()
     file.close()
 
-    # # Clean Hindi text
-    # def clean_hindi_text(text):
-    #     # Remove special characters but keep Hindi characters and basic punctuation
-    #     import re
-    #     # Keep Hindi characters (0900-097F), numbers, basic punctuation
-    #     cleaned = re.sub(r'[^।-॥\u0900-\u097F\s.,!?0-9]', '', text)
-    #     # Remove extra whitespace
-    #     cleaned = ' '.join(cleaned.split())
-    #     return cleaned
-
-    # script = clean_hindi_text(script)
-
     SAMPLE_FILE_NAME = "audio_tts.wav"
     VIDEO_SERVER = "pexel"
 
@@ -49,20 +37,20 @@ if __name__ == "__main__":
     #     voice = "en-AU-WilliamNeural"
     asyncio.run(generate_audio(script, SAMPLE_FILE_NAME, voice))
 
-    # timed_captions = generate_timed_captions(SAMPLE_FILE_NAME)
-    # print(timed_captions)
+    timed_captions = generate_timed_captions(SAMPLE_FILE_NAME)
+    print(timed_captions)
 
-    # search_terms = getVideoSearchQueriesTimed(scriptText, timed_captions)
-    # print(search_terms)
+    search_terms = getVideoSearchQueriesTimed(script, timed_captions)
+    print(search_terms)
 
-    # background_video_urls = None
-    # if search_terms is not None:
-    #     background_video_urls = generate_video_url(search_terms, VIDEO_SERVER)
-    #     print(background_video_urls)
-    # else:
-    #     print("No background video")
+    background_video_urls = None
+    if search_terms is not None:
+        background_video_urls = generate_video_url(search_terms, VIDEO_SERVER)
+        print(background_video_urls)
+    else:
+        print("No background video")
 
-    # background_video_urls = merge_empty_intervals(background_video_urls)
+    background_video_urls = merge_empty_intervals(background_video_urls)
 
     # if background_video_urls is not None:
     #     video = get_output_media(SAMPLE_FILE_NAME, timed_captions, background_video_urls, VIDEO_SERVER)
