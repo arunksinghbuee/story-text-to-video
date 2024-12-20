58ba8a48237eb38a3f998e2a0e31204068bf3088
Arun Kumar Singh <arunkumar.singh@bt.com>
2024-12-20 06:43:00 +0530

    Changing model to large-v3

--- utility/captions/timed_captions_generator.py
+++ utility/captions/timed_captions_generator.py
@@ -2,7 +2,7 @@ import whisper_timestamped as whisper
 from whisper_timestamped import load_model, transcribe_timestamped
 import re
 
-def generate_timed_captions(audio_filename,model_size="turbo"):
+def generate_timed_captions(audio_filename,model_size="large-v3"):
     WHISPER_MODEL = load_model(model_size)
    
     gen = transcribe_timestamped(WHISPER_MODEL, audio_filename, verbose=False, fp16=False)
